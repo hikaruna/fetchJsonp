a0d5f74a6f6b9af4f35534b581caa40b78f70d3c
hikaruna <hikaruna@users.noreply.github.com>
2024-05-07 03:56:36 +0900

    Update fetchJsonp.js
    jsDocを加筆

--- fetchJsonp.js
+++ fetchJsonp.js
@@ -2,6 +2,15 @@ import jsonp from 'https://esm.run/jsonp';
 
 /**
  * jsonpに対応したURLを指定してfetchし、jsonデータを取得します
+ * 
+ * 動作は https://github.com/webmodules/jsonp に準じます
+ * 
+ * Usage:
+ * import fetchJsonp from 'https://cdn.jsdelivr.net/gh/hikaruna/fetchJsonp/fetchJsonp.js';
+ * try {
+ * const jsonData = await fetchJsonp('https://example.com/jsonp_endpoint');
+ * } catch (err) { }
+ * 
  * @param url {string}
  * @returns {Promise<object>} fetched json data.
  */
